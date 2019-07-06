f3a2fe2242f845601934cf3c9aaed05f02284043
Alexandra Kim <akim@appulate.com>
2019-07-06 08:43:58 +0500

    draft work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var result = {};
+    var count;
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    
+    if (currency >= 50){
+        count = Math.floor(currency / 50);
+        currency -= count * 50;
+        result.H = count;
+    }
+    if (currency >= 25){
+        count = Math.floor(currency / 25);
+        currency -= count * 25;
+        result.Q = count;
+    }
+    if(currency >= 10) {
+        count = Math.floor(currency / 10);
+        currency -= count * 10;
+        result.D = count;
+    }
+    if(currency >= 5){
+        count = Math.floor(currency / 5);
+        currency -= count * 5;
+        result.N = count;
+    }
+    if(currency >= 1){
+        count = Math.floor(currency / 1);
+        currency -= count * 1;
+        result.P = count;
+    }
+    return result;
+
 }
